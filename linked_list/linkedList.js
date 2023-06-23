112e920b4b882f26346ddc7c6a70025330365556
Amanuel Dereje <eamand518@gmail.com>
2023-06-23 07:24:38 +0300

    added pop shift and unshift methods for the linked list

--- linked_list/linkedList.js
+++ linked_list/linkedList.js
@@ -12,7 +12,7 @@ class LinkedList {
         const newNode = new Node(value);
         if (!this.Head) {
             this.Head = newNode;
-            this.Tail = this.Head;
+            this.Tail = newNode;
         } else {
             this.Tail.next = newNode;
             this.Tail = newNode;
@@ -20,10 +20,59 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    Pop() {
+        if (!this.Head) {
+            return undefined;
+        } else {
+            let temp = this.Head;
+            let pre = this.Head;
+            while (temp.next) {
+                pre = temp;
+                temp = temp.next;
+            }
+            this.Tail = pre;
+            this.Tail.next = null;
+            this.length--;
+            if (this.length === 0) {
+                //if the list hade only single node
+                this.Head = null;
+                this.temp = null;
+            }
+            return temp;
+        }
+    }
+
+    Unsift(value) {
+        const newNode = new Node(value);
+        if (!this.Head) {
+            this.Head = newNode;
+            this.Tail = newNode;
+        } else {
+            newNode.next = this.Head;
+            this.Head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    Shift() {
+        if (!this.Head) {
+            return undefined;
+        } else if (this.length === 1) {
+            this.Head = null;
+            this.Tail = null;
+        } else {
+            let temp = this.Head.next;
+            this.Head = temp;
+            this.length--;
+
+            return temp;
+        }
+    }
 }
 
 const linklist = new LinkedList(11);
 linklist.Push(5);
 linklist.Push(4);
-linklist.Push(3);
 console.log(linklist);
