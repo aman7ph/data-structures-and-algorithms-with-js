545457ba4ce6779b1b54870753b48c8c871a995d
Amanuel Dereje <eamand518@gmail.com>
2023-06-28 23:50:03 +0300

    started and done stack practice

---
+++ stack/stack.js
@@ -0,0 +1,35 @@
+import Node from './node';
+
+class Stack {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.Top = newNode;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Stack(value);
+        if (this.length === 0) {
+            this.Top = newNode;
+        } else {
+            newNode.next = this.Top;
+            this.Top = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (this.length === 0) {
+            return undefined;
+        } else {
+            let temp = this.Top;
+            this.Top = temp.next;
+            this.Top = this.Top.next;
+            temp.next = null;
+
+            this.length--;
+            return temp;
+        }
+    }
+}
