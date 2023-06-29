6511d7f5a96ae1b1a6c5bf9dc60bc57a12cb539a
Amanuel Dereje <eamand518@gmail.com>
2023-06-29 20:57:01 +0300

    started and finished queue practicing

---
+++ queue/queue.js
@@ -0,0 +1,40 @@
+import Node from './node';
+
+class Queue {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.First = newNode;
+        this.Last = newNode;
+
+        this.length = 1;
+    }
+
+    enqueue(value) {
+        const newNode = new Queue(value);
+        if (this.length === 0) {
+            this.First = newNode;
+            this.Last = newNode;
+        } else {
+            this.Last.next = newNode;
+            this.Last = newNode;
+        }
+        this.length++;
+
+        return this;
+    }
+    dequeue() {
+        if (this.length === 0) {
+            return undefined;
+        } else {
+            let temp = this.First;
+            this.First = this.First.next;
+            temp.next = null;
+            this.length--;
+            if (this.length === 0) {
+                this.First = null;
+                this.Last = null;
+            }
+            return temp;
+        }
+    }
+}
